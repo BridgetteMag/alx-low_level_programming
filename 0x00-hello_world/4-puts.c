0ef56dd70175e6fabb6dadb98e2a599ef2a21dcc
BridgetteMag <motshidisimagabane>
2022-09-09 02:51:14 -0700

    Task4 hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#!/bin/bash
-#include<studio>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: 'A programme that prints a string put'
- *
- * Return: Always 0 (Sucessful)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
-	return(0);
+	puts('\"Programming is like building a multilingual puzzle");
+	return (0);
 }
