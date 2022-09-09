2430a43f2a78f114500daf2c5e2bd0f2b1fc2feb
BridgetteMag <motshidisimagabane>
2022-09-09 06:08:42 -0700

    Bonus task1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,13 @@
  * Return: 0
  */
 int main(void)
-{
 
-	printf("Size of a char %lu byte(s)\n", sizeof(char));
-	printf("Size of an int %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long in %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float %lu byte(s)\n", sizeof(float));
+{
+	
+	printf("size of an int %lu.\n", (int)sizeof(4));
+	printf("size of a char %lu.\n", (char)sizeof(1));
+	printf("size of a long int %lu.\n", (long int)sizeof(4));
+	printf(size of a long long int %lu.\n", (long long int)sizeof(8));
+	printf(size of a float %lu.\n", (float)sizeof(4));
 	return (0);
 }
