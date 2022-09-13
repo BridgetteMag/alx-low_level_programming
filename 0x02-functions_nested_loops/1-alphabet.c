55f186a64afc9ff7aadbc4a108d968b95b5ab294
BridgetteMag <motshidisimagabane>
2022-09-13 02:45:34 -0700

    Task1 alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Descripotion: prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+		return (0);
+}
