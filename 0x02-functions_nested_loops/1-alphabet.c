a9123eb2b4fe0ace403d5a2c7a55c4feecfafe67
BridgetteMag <motshidisimagabane>
2022-09-14 00:20:40 -0700

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - main entry
  *
@@ -12,8 +12,8 @@ int main(void)
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		putchar(ch);
+		_putchar(ch);
 	}
-		putchar('\n');
+		_putchar('\n');
 		return (0);
 }
