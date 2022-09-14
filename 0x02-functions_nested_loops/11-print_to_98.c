fc0462c0187bdbb649332adc7ae9f20d93e26648
BridgetteMag <motshidisimagabane>
2022-09-14 03:12:34 -0700

    Task 11 Battery street the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from input to 98
+ * print_to_98 - Prints all natural numbers from input to 98,
+ * in order seperated by a comma followedby a space.
  * @n: The number to begin counting at.
  */
 void print_to_98(int n)
@@ -13,9 +14,11 @@ void print_to_98(int n)
 			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
+
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
