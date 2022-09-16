a593bdee98f642c610df8e48c8b2114db7d70395
BridgetteMag <motshidisimagabane>
2022-09-16 08:39:01 -0700

    Task9 fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,36 +7,26 @@
  */
 int main(void)
 {
-	int num = 1;
+	int num;
 
-	while (num++ < 100)
+	for (num = 1; num <= 100; num++)
+
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz");
-	}
-	else if ((num % 3) == 0)
-	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+
 			printf("Fizz");
-	}
-	else if ((num % 5) == 0)
-	{
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
 		if (num != 100)
-	{
-		printf ("Buzz");
-	}
-	else
-		{
-		printf("Buzz");
-		}
-	}
-	else
-	{
-		printf("%d", num);
-	}
+			printf(" ");
+		else
 
-	}
 	printf("\n");
+}
 
 	return (0);
 
