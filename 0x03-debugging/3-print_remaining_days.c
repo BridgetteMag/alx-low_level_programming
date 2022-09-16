579e7d6e3178633bdb35ee6ffc91d8dab3fb34e6
BridgetteMag <motshidisimagabane>
2022-09-16 07:40:10 -0700

    Task3-remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,15 +19,15 @@ if (month >= 3 && day >= 60)
 day++;
 }
 
-printf( "Day of the year: %d\n ", day);
-printf( "Remaining days: %d\n ", 366 - day);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
 if (month == 2 && day == 60)
 {
-printf( "Day of the year: %d\n ", day);
-printf( "Remaining days: %d\n ", 365 - day);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
