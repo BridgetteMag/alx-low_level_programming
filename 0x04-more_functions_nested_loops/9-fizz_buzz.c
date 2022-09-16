d06ea891608565523763b1181b225abc634653c8
BridgetteMag <motshidisimagabane>
2022-09-16 04:16:45 -0700

    Task9 fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,28 +11,28 @@ int main(void)
 
 	while (num++ < 100)
 	{
-	if ((num % 3 == 0) && (num % 5 == 0))
-	{
-		printf("FizzBuzz ");
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz");
 	}
 	else if ((num % 3) == 0)
 	{
-			printf("Fizz ");
+			printf("Fizz");
 	}
 	else if ((num % 5) == 0)
 	{
-	if (num != 100)
+		if (num != 100)
 	{
-	printf("Buzz ");
+		printf ("Buzz");
+	}
+	else
+		{
+		printf("Buzz");
+		}
 	}
 	else
 	{
-	printf("Buzz ");
-	}
-	}
-	else
-	{
-		printf("%d ", num);
+		printf("%d", num);
 	}
 
 	}
--- 0x04-more_functions_nested_loops/README.c
+++
@@ -1,1 +0,0 @@
-Write a function that draws a diagonal line in the terminal
