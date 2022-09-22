812e024f9dbb9b4f180536f6bb869fc9756f4cbb
BridgetteMag <motshidisimagabane>
2022-09-22 00:20:50 -0700

    Task0 strcat

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - functions that compares two strings
+ * @s1: first param
+ * @s2: second param
+ * Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	strcmp(s1, s2);
+	return (result);
+}
