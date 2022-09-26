1f2e0249d6daab28706ce21f37a1b36c6c58a604
BridgetteMag <motshidisimagabane>
2022-09-26 05:52:18 -0700

    Task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,13 +5,13 @@
  * @s: pointed destination
  * @b: constant byte
  * @n: bytes
- * Return: Always 0 (Success)
+ * Return:the memory area filled
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	for (a = 0; a < n; a++)
+		s[a] = b;
 return (s);
 }
