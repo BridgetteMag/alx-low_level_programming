afd969d66d8afd88994b7a1314729d364e79a78e
BridgetteMag <motshidisimagabane>
2022-09-28 01:26:47 -0700

    Bonus task - Inceptin, now before you bother, telling me its impossible.

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings
+ * @s1: String to be compared
+ * @s2: String to be compared
+ *
+ * Return: 1 if considered identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
