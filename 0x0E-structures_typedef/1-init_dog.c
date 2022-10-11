ca5d313dcf7a371220d144e76918603cc63b9a42
BridgetteMag <motshidisimagabane>
2022-10-10 21:11:13 -0700

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * init_dog - Initializes a dog structure
- * @d: A dog structure
+ * @d: Array
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The owner of the dog
@@ -13,6 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
+		return;
+
+	else
 	{
 		d->name = name;
 		d->age = age;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,6 @@ struct dog
 
 };
 
-/**
- * dog_t - Type for dog structure
- */
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name. float age, char *owner);
 void print_dog(struct dog *d);
