c9e0286013385d8e7ee3a49e77caf781876a85c9
BridgetteMag <motshidisimagabane>
2022-10-12 00:05:13 -0700

    Task0-Whats my name?

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "functions_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: The name to print
+ * @f: Pointer to function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+		f(name);
+}
