ccf4800cfb732f1b50eb78287abb0ed5cc094b50
BridgetteMag <motshidisimagabane>
2022-12-15 00:23:25 -0800

    Task0-A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copies a string
+ * @dest: 1st param
+ * @src: 2nd param
+ * @n: 3rd param
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy paste string
+ * @dest: destinantion
+ * @src: source
+ *
+ * Return: destinantion
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a, b = 0;
+
+	for (a = 0; src[a] != '\0'; a++)
+
+	{
+		dest[b] = src[a];
+			b++;
+	}
+	dest[b] = '\0';
+
+	return (dest);
+}
